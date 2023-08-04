af7e03fe3c9d38b6d7e2fbf62973c228464aa27d
EnZzzzzz <957078942@qq.com>
2023-08-05 00:13:41 +0800

    修改图片

--- manage/case/show_case.py
+++ manage/case/show_case.py
@@ -26,17 +26,19 @@ class ShowCase:
 
     def _lazy_init_layout(self):
         if self._body is empty:
-            all_case = db.get_all_case()
+            self._body = self._create_primary()
 
-            body_items = []
-            while all_case:
-                row_items = []
-                for _ in range(min(self.col_num, len(all_case))):
-                    case = all_case.pop()
-                    case_body = self._create_card(case)
-                    row_items.append(case_body)
-                body_items.append(dbc.Row(row_items, className="mb-3"))
-            self._body = html.Div(body_items)
+    def _create_primary(self):
+        all_case = db.get_all_case()
+        body_items = []
+        while all_case:
+            row_items = []
+            for _ in range(min(self.col_num, len(all_case))):
+                case = all_case.pop()
+                case_body = self._create_card(case)
+                row_items.append(case_body)
+            body_items.append(dbc.Row(row_items, className="mb-3"))
+        return html.Div(body_items)
 
     @staticmethod
     def _create_card(case: Case):
@@ -50,9 +52,11 @@ class ShowCase:
                 dbc.CardBody([
                     dbc.Row([
                         dbc.Col([html.Img(src=logo, style={"width": "10rem"})]),
-                        dbc.Col([html.H4(title, className=f"{case.name}-card-title"),
-                                 html.P(desc, className=f"{case.name}-card-text"),
-                                 html.Small(case.name, className="card-text text-muted")])
+                        dbc.Col([
+                            html.H4(title, className=f"{case.name}-card-title"),
+                            html.P(desc, className=f"{case.name}-card-text"),
+                            html.Small(case.name, className="card-text text-muted")
+                        ])
                     ])
                 ]),
             ],
